dc5891da545463433df709ad9dee3d54a30eb96b
Denis M <tancuipokamolodoi@gmail.com>
2026-07-17 17:19:24 +0300

    header block + bgi + buttonChat + navigation complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { Header } from "./components/Header/Header";
 import { Hero } from "./components/Hero/Hero";
-import { IntroSection } from "./components/IntroSection/IntroSection";
-import "./styles/App.scss";
 
 function App() {
   return (
-    <>
-      <div className="heroSection">
-        <Hero />
-        <Header />
-      </div>
-      <IntroSection />
-    </>
+    <div className="heroSection">
+      <Hero />
+      <Header />
+    </div>
   );
 }
 
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,13 +1,16 @@
 import styles from "./Hero.module.scss";
-import wavesVideo from "../../assets/wavesSprite/vawesVideo_(24).mp4";
+import buttImg from "../../assets/buttconicons/butIcon.png";
 
 export const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <video autoPlay loop muted playsInline className={styles.waveVideo}>
-        <source src={wavesVideo} type="video/mp4" />
-      </video>
-      <h1 className={styles.title}>FREEDOM SURF</h1>
-    </div>
+    <>
+      <section className={styles.hero}>
+        <h1 className={styles.title}>FREEDOM SURF</h1>
+        <button className={styles.swellButton}>
+          Join SwellChat
+          <img src={buttImg} alt="Buttom Image" className={styles.img} />
+        </button>
+      </section>
+    </>
   );
 };
