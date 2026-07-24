b8f3864c7adf188597cfe668d06e8ecb1da08d7e
Denis M <tancuipokamolodoi@gmail.com>
2026-07-24 11:52:07 +0300

    fix calendar error

--- src/components/SurfCalendar/SurfCalendar.tsx
+++ src/components/SurfCalendar/SurfCalendar.tsx
@@ -1,6 +1,6 @@
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
-import "./SurfCalendar.module.scss";
+import styles from "./SurfCalendar.module.scss";
 
 interface SurfCalendarProps {
   selected: Date | undefined;
@@ -9,16 +9,18 @@ interface SurfCalendarProps {
 
 export const SurfCalendar = ({ selected, onSelect }: SurfCalendarProps) => {
   return (
-    <DayPicker
-      mode="single"
-      selected={selected}
-      onSelect={onSelect}
-      weekStartsOn={1}
-      showOutsideDays={true}
-      // defaultMonth={new Date(2026, 6)} // июль
-      // startMonth={new Date(2026, 6)} // вместо fromMonth
-      // endMonth={new Date(2026, 6)}
-      disableNavigation={true}
-    />
+    <div className={styles.wrapper}>
+      <DayPicker
+        mode="single"
+        selected={selected}
+        onSelect={onSelect}
+        weekStartsOn={1}
+        showOutsideDays={true}
+        // defaultMonth={new Date(2026, 6)} // июль
+        // startMonth={new Date(2026, 6)} // вместо fromMonth
+        // endMonth={new Date(2026, 6)}
+        disableNavigation={true}
+      />
+    </div>
   );
 };
